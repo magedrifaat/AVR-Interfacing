9ef4de8a404e3b88132791524319d66370e1ebab
maged <magedrifaat@gmail.com>
2021-09-12 12:11:58 +0200

    Implemented LED Patterns Task

--- APP/main.c
+++ APP/main.c
@@ -14,37 +14,94 @@
 
 #include <util/delay.h>
 
-#define LED1_PIN  DIO_u8_PIN_D0
-#define LED2_PIN  DIO_u8_PIN_D1
+#define LED_PORT  DIO_u8_PORTC
 
-#define SW1_PIN    DIO_u8_PIN_D2
-#define SW2_PIN    DIO_u8_PIN_D3
+#define SW_PIN    DIO_u8_PIN_D2
 
-void sw1_handler(void);
-void sw2_handler(void);
+void sw_handler(void);
+
+void voidBlink(u8 Copy_u8Step);
+void voidSweepDown(u8 Copy_u8Step);
+void voidSweepUp(u8 Copy_u8Step);
+void voidFillDown(u8 Copy_u8Step);
+void voidFillUp(u8 Copy_u8Step);
+void voidInsideOut(u8 Copy_u8Step);
+void voidOutsideIn(u8 Copy_u8Step);
+void voidVibrate(u8 Copy_u8Step);
+
+const u8 u8PATTERN_COUNT = 8;
+void (*pvoidPatterns[])(u8 Copy_u8Step) = {voidBlink, voidSweepDown, voidSweepUp, voidFillDown, \
+										   voidFillUp, voidInsideOut, voidOutsideIn, voidVibrate};
+
+volatile u8 u8CurrentPattern;
+volatile u8 u8CurrentStep;
 
 int main(void) {
-	DIO_voidSetPinDirection(LED1_PIN, DIO_u8_OUTPUT);
-	DIO_voidSetPinDirection(LED2_PIN, DIO_u8_OUTPUT);
+	u8CurrentPattern = 0;
+	u8CurrentStep = 0;
 
-	DIO_voidActivatePullUp(SW1_PIN);
-	DIO_voidActivatePullUp(SW2_PIN);
+	DIO_voidSetPortDirection(LED_PORT, DIO_u8_PORT_OUTPUT);
 
-	EXTI_voidEnable(EXTI_u8_LINE0, EXTI_u8_ON_CHANGE);
-	EXTI_voidEnable(EXTI_u8_LINE1, EXTI_u8_ON_CHANGE);
-	EXTI_voidCallBack(sw1_handler, EXTI_u8_LINE0);
-	EXTI_voidCallBack(sw2_handler, EXTI_u8_LINE1);
+	DIO_voidActivatePullUp(SW_PIN);
+
+	EXTI_voidEnable(EXTI_u8_LINE0, EXTI_u8_RISING_EDGE);
+	EXTI_voidCallBack(sw_handler, EXTI_u8_LINE0);
 	GIE_voidEnableInterrupts();
 
 	while (1) {
+		pvoidPatterns[u8CurrentPattern](u8CurrentStep++);
 	}
 }
 
-
-void sw1_handler(void) {
-	DIO_voidTogglePin(LED1_PIN);
+void sw_handler(void) {
+	u8CurrentPattern = (u8CurrentPattern + 1) % u8PATTERN_COUNT;
+	u8CurrentStep = 0;
 }
 
-void sw2_handler(void) {
-	DIO_voidTogglePin(LED2_PIN);
+void voidBlink(u8 Copy_u8Step) {
+	u8 LOC_u8Step = Copy_u8Step % 2;
+	DIO_voidSetPortValue(LED_PORT, 0xFF * LOC_u8Step);
+	_delay_ms(500);
+}
+
+void voidSweepDown(u8 Copy_u8Step) {
+	u8 LOC_u8Step = Copy_u8Step % 9;
+	DIO_voidSetPortValue(LED_PORT, 1 << LOC_u8Step);
+	_delay_ms(50);
+}
+
+void voidSweepUp(u8 Copy_u8Step) {
+	u8 LOC_u8Step = Copy_u8Step % 9;
+	DIO_voidSetPortValue(LED_PORT, 0x80 >> LOC_u8Step);
+	_delay_ms(50);
+}
+
+void voidFillDown(u8 Copy_u8Step) {
+	u8 LOC_u8Step = Copy_u8Step % 9;
+	DIO_voidSetPortValue(LED_PORT, 0xFF >> (8 - LOC_u8Step));
+	_delay_ms(50);
+}
+
+void voidFillUp(u8 Copy_u8Step) {
+	u8 LOC_u8Step = Copy_u8Step % 9;
+	DIO_voidSetPortValue(LED_PORT, 0xFF << (8 - LOC_u8Step));
+	_delay_ms(50);
+}
+
+void voidInsideOut(u8 Copy_u8Step) {
+	u8 LOC_u8Step = Copy_u8Step % 5;
+	DIO_voidSetPortValue(LED_PORT, (0x0F >> LOC_u8Step) | (0xF0 << LOC_u8Step));
+	_delay_ms(100);
+}
+
+void voidOutsideIn(u8 Copy_u8Step) {
+	u8 LOC_u8Step = Copy_u8Step % 5;
+	DIO_voidSetPortValue(LED_PORT, (0x0F >> (4 - LOC_u8Step)) | (0xF0 << (4 - LOC_u8Step)));
+	_delay_ms(100);
+}
+
+void voidVibrate(u8 Copy_u8Step) {
+	u8 LOC_u8Step = Copy_u8Step % 2;
+	DIO_voidSetPortValue(LED_PORT, 0x55 << LOC_u8Step);
+	_delay_ms(200);
 }
